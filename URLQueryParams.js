6f9d3005c84b553fc1a8b7c9d51ac4cb3a37570d
prodvair <prodvair.almaz@yandex.ru>
2024-10-29 18:48:35 +0300

    fixed array parsing and now alias work with value

--- URLQueryParams.js
+++ URLQueryParams.js
@@ -1,167 +1,148 @@
 function isNumber(str) {
-    return str == Number(str);
+  return str == Number(str);
 }
 
 export class URLQueryParams {
-    constructor(queryAlias) {
-        if (queryAlias) this.constructor.queryAlias == queryAlias;
-    }
+  constructor(queryAlias) {
+    if (queryAlias) this.constructor.queryAlias == queryAlias;
+  }
 }
+export const DEFAULT_ALIAS = {
+  filters: 'f',
+  wheres: 'w',
+  orders: 'o',
+  groupe: 'g',
+  column: 'col',
+  condition: 'con',
+  value: 'val',
+  option: 'opt',
+};
 
 Object.defineProperty(URLQueryParams.prototype, '$queryParams', {
-    get() {
-        return this.constructor._queryParams;
-    },
+  get() {
+    return this.constructor._queryParams;
+  },
 });
 URLQueryParams.url = null;
 URLQueryParams._queryParams = {};
-URLQueryParams.queryAlias = {
-    filters: 'f',
-    wheres: 'w',
-    orders: 'o',
-    groupe: 'g',
-    column: 'col',
-    condition: 'con',
-    value: 'val',
-    option: 'opt',
-};
+URLQueryParams.queryAlias = DEFAULT_ALIAS;
 
 URLQueryParams.getThisUrl = function (url) {
-    var urlParams = document.location.search;
-    if ('string' === typeof url) urlParams = new URL(url).search;
-    const searchParams = new URLSearchParams(urlParams);
-    const obj = {};
-    for (const [key, value] of searchParams.entries()) {
-        obj[key] = value;
-    }
-    return obj;
+  var urlParams = document.location.search;
+  if ('string' === typeof url) urlParams = new URL(url).search;
+  const searchParams = new URLSearchParams(urlParams);
+  const obj = {};
+  for (const [key, value] of searchParams.entries()) {
+    obj[key] = value;
+  }
+  return obj;
 };
 
 URLQueryParams.convertNumber = function (str) {
-    return isNumber(str) ? Number(str) : str;
+  return isNumber(str) ? Number(str) : str;
 };
 
 URLQueryParams.convert = function (value) {
-    if (value === null) return 'null';
-    if (value === false) return 'false';
-    if (value === true) return 'true';
-    if (value === 'null') return null;
-    if (value === 'false') return false;
-    if (value === 'true') return true;
-    if (value === '') return '';
-    return this.convertNumber(value);
+  if (value === null) return 'null';
+  if (value === false) return 'false';
+  if (value === true) return 'true';
+  if (value === 'null') return null;
+  if (value === 'false') return false;
+  if (value === 'true') return true;
+  if (value === '') return '';
+  return this.convertNumber(value);
 };
 
 URLQueryParams.getAliasName = function (name) {
-    return this.queryAlias?.[name] || name;
+  return this.queryAlias?.[name] || name;
 };
 
 URLQueryParams.parseAliasName = function (name) {
-    Object.entries(this.queryAlias).forEach(([key, value]) => {
-        if (value === name) name = key;
-    });
-    return name;
+  Object.entries(this.queryAlias).forEach(([key, value]) => {
+    if (value === name) name = key;
+  });
+  return name;
 };
 
 URLQueryParams.queryParamsBuild = function (
-    params,
-    name = undefined,
-    staples = false
+  params,
+  name = undefined,
+  staples = false
 ) {
-    var string = '';
-    if ([null, undefined, ''].includes(params)) return '';
+  var string = '';
+  if ([null, undefined, ''].includes(params)) return '';
 
-    name = this.getAliasName(name);
+  name = this.getAliasName(name);
 
-    if (typeof params === 'object' && params !== null) {
-        if (Array.isArray(params)) {
-            params.forEach((item, key) => {
-                const keyName = staples
-                    ? `${name}[${params.length !== 1 ? key : ''}]`
-                    : name;
-                string += this.queryParamsBuild(item, keyName, true);
-            });
-        } else {
-            Object.keys(params).forEach((key) => {
-                const item = params[key];
-                const keyName = staples
-                    ? `${name}[${this.getAliasName(key)}]`
-                    : key;
-                string += this.queryParamsBuild(item, keyName, true);
-            });
-        }
+  if (typeof params === 'object' && params !== null) {
+    if (Array.isArray(params)) {
+      params.forEach((item, key) => {
+        const keyName = staples
+          ? `${name}[${params.length !== 1 ? key : ''}]`
+          : name;
+        string += this.queryParamsBuild(item, keyName, true);
+      });
     } else {
-        string += `${name}=${this.convert(params)}&`;
+      Object.keys(params).forEach((key) => {
+        const item = params[key];
+        const keyName = staples ? `${name}[${this.getAliasName(key)}]` : key;
+        string += this.queryParamsBuild(item, keyName, true);
+      });
     }
+  } else {
+    string += `${name}=${this.getAliasName(this.convert(params))}&`;
+  }
 
-    if (!staples) {
-        string = string.slice(0, -1);
-    }
+  if (!staples) {
+    string = string.slice(0, -1);
+  }
 
-    return string;
+  return string;
 };
-URLQueryParams.prototype.queryParamsBuild = function (
-    params,
-) {
-    return this.constructor.queryParamsBuild(params);
+URLQueryParams.prototype.queryParamsBuild = function (params) {
+  return this.constructor.queryParamsBuild(params);
 };
 
 URLQueryParams.queryParamsParse = function (_queryParams = null, url = null) {
-    if (!_queryParams) _queryParams = this.getThisUrl(url);
-    var obj;
+  if (!_queryParams) _queryParams = this.getThisUrl(url);
+  var obj;
 
-    Object.keys(_queryParams).forEach((key) => {
-        const params = key.match(/([a-zA-Z0-9]+)|\[[a-zA-Z0-9]*\]/g);
-        const value = this.convert(_queryParams[key]);
+  Object.keys(_queryParams).forEach((key) => {
+    const params = key.match(/([a-zA-Z0-9]+)|\[[a-zA-Z0-9]*\]/g);
+    const value = this.parseAliasName(this.convert(_queryParams[key]));
 
-        const recursParse = (names, value, subObj, i = 0) => {
-            if (!names[i]) return value;
-            const thisName = this.parseAliasName(
-                this.convertNumber(names[i].replace(/\[|\]/g, '')) || 0
-            );
-            if (!thisName && thisName !== 0) return value;
-            if (subObj?.[thisName]) {
-                subObj[thisName] = recursParse(
-                    names,
-                    value,
-                    subObj[thisName],
-                    i + 1
-                );
-            }
+    const recursParse = (names, value, subObj, i = 0) => {
+      if (!names[i]) return value;
+      const thisName = this.parseAliasName(
+        this.convertNumber(names[i].replace(/\[|\]/g, '')) || 0
+      );
+      if ([undefined, null].includes(thisName)) return value;
+      if (subObj?.[thisName]) {
+        subObj[thisName] = recursParse(names, value, subObj[thisName], i + 1);
+      }
 
-            if (typeof subObj !== 'object') {
-                subObj = isNumber(thisName) ? new Array() : {};
-            }
+      if (typeof subObj !== 'object') {
+        subObj = isNumber(thisName) ? new Array() : {};
+      }
 
-            if (!Array.isArray(subObj) || subObj[thisName]) {
-                subObj[thisName] = recursParse(
-                    names,
-                    value,
-                    subObj[thisName],
-                    i + 1
-                );
-            } else {
-                subObj.push(recursParse(names, value, subObj[thisName], i + 1));
-            }
-            return subObj;
-        };
+      subObj[thisName] = recursParse(names, value, subObj[thisName], i + 1);
+      return subObj;
+    };
 
-        if (params[0] && !obj) {
-            obj = isNumber(
-                this.convertNumber(params[0].replace(/\[|\]/g, '')) || 0
-            )
-                ? new Array()
-                : {};
-        }
+    if (params[0] && !obj) {
+      obj = isNumber(this.convertNumber(params[0].replace(/\[|\]/g, '')) || 0)
+        ? new Array()
+        : {};
+    }
 
-        obj = recursParse(params, value, obj);
-    });
+    obj = recursParse(params, value, obj);
+  });
 
-    this._queryParams = obj;
+  this._queryParams = obj;
 
-    return obj;
+  return obj;
 };
 
 URLQueryParams.prototype.queryParamsParse = function (url = null) {
-    return this.constructor.queryParamsParse(null, url);
+  return this.constructor.queryParamsParse(null, url);
 };
